eb016582ccb037896e0a5cc888e7859264356bbe
Lucas Wojciechowski <lucas@lucaswoj.com>
2016-07-30 09:13:04 -0700

    Make sidebar code more react idiomatic

--- code/KallaxSidebarView.js
+++ code/KallaxSidebarView.js
@@ -4,12 +4,20 @@ const React = require('react');
 
 module.exports = class KallaxSidebarView extends React.Component {
 
+    state: { width: number; };
+
+    props: {};
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {width: 200};
+    }
+
     render() {
-        return <div className="KallaxSidebarView" ref="sidebar">
+        return <div className="KallaxSidebarView" style={{width: this.state.width}}>
 
             <div draggable className="resize-handle" onDrag={(event) => {
-                if (event.pageX === 0) return;
-                this.refs.sidebar.style.width = event.pageX + "px";
+                if (event.pageX !== 0) this.setState({width: event.pageX});
             }} />
 
             <h2>Pages</h2>
