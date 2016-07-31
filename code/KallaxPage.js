672bf7f10890eb038b8efb1cb7553d0da6923e68
Lucas Wojciechowski <lucas@lucaswoj.com>
2016-07-31 16:39:13 -0700

    Add file-back pages

---
+++ code/KallaxPage.js
@@ -0,0 +1,34 @@
+// @flow
+
+const {app} = require('electron').remote;
+const Path = require('path');
+const FS = require('fs');
+
+const pagesDir = Path.join(app.getAppPath(), 'data');
+
+module.exports = class KallaxPage {
+
+    static getPages() {
+        return FS.readdirSync(pagesDir).map(this.getPageFromFile.bind(this));
+    }
+
+    static getInitialActivePage() {
+        return this.getPages()[0];
+    }
+
+    static getPageFromFile(file) {
+        return new KallaxPage({
+            title: Path.basename(file, '.md'),
+            body: FS.readFileSync(Path.join(pagesDir, file), 'utf8')
+        });
+    }
+
+    title: string;
+    body: string;
+
+    constructor({title, body}: {title: string, body: string}) {
+        this.title = title;
+        this.body = body;
+    }
+
+};
--- code/KallaxView.js
+++ code/KallaxView.js
@@ -1,12 +1,55 @@
 // @flow
 
 const React = require('react');
-const KallaxSidebarView = require('./KallaxSidebarView');
+const KallaxPage = require('./KallaxPage');
 
 module.exports = class KallaxView extends React.Component {
 
+    state: {
+        sidebarWidth: number;
+        activePage: KallaxPage;
+    };
+
+    props: {};
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            sidebarWidth: 200,
+            activePage: KallaxPage.getInitialActivePage()
+        };
+    }
+
     render() {
-        return <KallaxSidebarView />;
+        return <div className="KallaxView">
+            {this.renderSidebar()}
+            {this.renderActivePage()}
+        </div>;
+    }
+
+    renderSidebar() {
+        return <div className="sidebar" style={{width: this.state.sidebarWidth}}>
+
+            <div draggable className="resize-handle" onDrag={(event) => {
+                if (event.pageX !== 0) this.setState({sidebarWidth: event.pageX});
+            }} />
+
+            <h2>Pages</h2>
+            <ul>{KallaxPage.getPages().map((page) => {
+                return <li
+                    key={page.title}
+                    onClick={() => { this.setState({activePage: page}); }}
+                    className={this.state.activePage.title === page.title ? 'active' : ''}
+                >
+                    {page.title}
+                </li>;
+            })}</ul>
+
+        </div>;
+    }
+
+    renderActivePage() {
+        return <div className="active-page">{this.state.activePage.body}</div>;
     }
 
 };
