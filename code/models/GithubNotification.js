6682abe68abbe3ef3eedaa41338c5b0cbf403306
Lucas Wojciechowski <lucas@lucaswoj.com>
2016-08-04 07:59:35 -0700

    Added the ability to traverse all pages of Github notifications

--- code/models/GithubNotification.js
+++ code/models/GithubNotification.js
@@ -2,6 +2,10 @@
 
 const Model = require('../Model');
 const ModelArray = require('../ModelArray');
+const _ = require('lodash');
+const Querystring = require('querystring');
+const assert = require('assert');
+const URL = require('url');
 
 type Props = {
     id: string;
@@ -19,22 +23,57 @@ type Props = {
     };
 };
 
+const PER_PAGE = 50;
+
 module.exports = class GithubNotification extends Model<Props> {
 
     static all: ModelArray<GithubNotification> = new ModelArray(() => {
 
-        const pages = [fetchPage(0)];
-
-        function fetchPage(index) {
-            return fetch(`https://api.github.com/notifications?access_token=${process.env.GITHUB_TOKEN}&page=${index}`)
-                .then((response) => response.json());
-        }
+        const fetchPage = _.memoize((index) =>
+            fetch('https://api.github.com/notifications?' + Querystring.stringify({
+                access_token: process.env.GITHUB_TOKEN,
+                per_page: PER_PAGE,
+                page: index,
+                all: false // only show unread notifications
+            })).then((response) =>
+                response.json().then((json) =>
+                    ({link: response.headers.get('Link'), items: json})
+                )
+            )
+        );
 
         return {
-            length: pages[0].then((page) => page.length),
-            fetch: (index: number): Promise<GithubNotification> =>
-                pages[0].then((page) => new GithubNotification(page[index]))
+            length: fetchPage(0).then((firstPage) => {
+                const lastPageIndex = parseLinkHeader(firstPage.link).last;
+                return fetchPage(lastPageIndex).then((lastPage) => {
+                    return lastPageIndex * PER_PAGE + lastPage.items.length
+                })
+            }),
+
+            fetch: _.memoize((index: number): Promise<GithubNotification> =>
+                fetchPage(Math.floor(index / PER_PAGE)).then((page) =>
+                    new GithubNotification(page.items[index % PER_PAGE])
+                )
+            )
         };
     });
 
 };
+
+function parseLinkHeader(header) {
+    assert(header.length);
+
+    const links = header.split(',');
+    const output = {};
+
+    for (let i = 0; i < links.length; i++) {
+        const link = links[i];
+        const linkParts = link.split(';');
+        assert(linkParts.length === 2);
+        const index = parseInt(URL.parse(linkParts[0].slice(1, -1), true).query.page);
+        const name = linkParts[1].replace(/rel="(.+)"/, '$1').trim();
+        output[name] = index;
+    }
+
+    return output;
+}
