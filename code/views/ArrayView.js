be434b955e3f56ebf5e7c4ea1d3efdd044f45326
Lucas Wojciechowski <lucas@lucaswoj.com>
2016-09-13 08:51:54 -0700

    Refactor ArrayView

--- code/views/ArrayView.js
+++ code/views/ArrayView.js
@@ -6,6 +6,7 @@ const PromiseView = require('./PromiseView');
 const LoadingView = require('./LoadingView');
 const ErroredView = require('./ErroredView');
 const bind = require('autobind-decorator');
+const _ = require('lodash');
 
 type Props<T> = {
     array: KallaxArray<T>;
@@ -22,79 +23,75 @@ module.exports = class ArrayView<T> extends React.Component<void, Props<T>, Stat
     props: Props<T>;
     state: State;
 
-    renderedElements: Array<React.Element>;
-    averageElementHeight: number;
-    containerElement: Element;
+    childViewCache: Array<React.Element>;
+    averageChildViewHeight: number = 100;
+    element: Element;
 
     constructor(props: Props<T>) {
         super(props);
         this.state = {endIndex: 20};
 
-        this.renderedElements = [];
+        this.childViewCache = [];
         this.props.array.on('change', () => {
-            this.renderedElements = [];
+            this.childViewCache = [];
             this.forceUpdate();
         });
     }
 
     render() {
-        if (this.containerElement) {
-            const height = Array.prototype.reduce.call(this.containerElement.childNodes, (sum, element) => sum + element.offsetHeight, 0);
-            this.averageElementHeight = height / this.containerElement.childNodes.length;
-        } else {
-            this.averageElementHeight = 200;
+        if (this.childViewCache.length < this.state.endIndex) {
+            for (let i = this.childViewCache.length; i < this.state.endIndex; i++) {
+                this.childViewCache[i] = <PromiseView
+                    key={i}
+                    promise={this.props.array.get(i)}
+                    render={this.props.renderElement}
+                    renderLoading={() => <div style={{height: this.averageChildViewHeight}}><LoadingView /></div>}
+                    renderErrored={() => <div style={{height: this.averageChildViewHeight}}><ErroredView /></div>}
+                />;
+            }
         }
-
-        for (let i = this.renderedElements.length - 1; i < this.state.endIndex; i++) {
-            this.renderedElements[i] = <PromiseView
-                key={i}
-                promise={this.props.array.get(i)}
-                render={this.props.renderElement}
-                renderLoading={this.renderLoadingElement}
-                renderErrored={this.renderErroredElement}
-            />;
-        }
-
-        const height = this.state.length == null ? '200%' : this.state.length * this.averageElementHeight;
 
         return (
             <div
-                style={{height: height}}
-                ref={(element) => { this.containerElement = element; }}
-            >{this.renderedElements}</div>
+                style={{height: this.state.length == null ? '200%' : this.state.length * this.averageChildViewHeight}}
+                ref={(element) => { this.element = element; }}
+            >{this.childViewCache}</div>
         );
 
     }
 
     @bind
-    renderLoadingElement() {
-        return <div style={{height: this.averageElementHeight}}><LoadingView /></div>;
-    }
+    onViewportUpdate() {
+        if (this.element && this.state.length !== undefined) {
+            const visibleHeight = window.innerHeight - this.element.getBoundingClientRect().top;
+            this.setState({
+                endIndex: _.clamp(
+                    Math.ceil(visibleHeight / this.averageChildViewHeight),
+                    this.state.endIndex,
+                    this.state.length || 0
+                )
+            });
 
-    @bind
-    renderErroredElement() {
-        return <div style={{height: this.averageElementHeight}}><ErroredView /></div>;
+            const childNodes = this.element.childNodes;
+            const sampleCount = Math.min(childNodes.length, 100);
+            let sampleHeightSum = 0;
+            for (let i = 0; i < sampleCount; i++) {
+                const sampleIndex = _.random(0, childNodes.length - 1);
+                sampleHeightSum += ((childNodes[sampleIndex]: any): Element).getBoundingClientRect().height;
+            }
+            this.averageChildViewHeight = sampleHeightSum / sampleCount;
+        }
     }
 
     componentDidMount() {
         this.props.array.length.then((length) => this.setState({length: length}));
-        window.addEventListener('scroll', this.onScroll);
+        window.addEventListener('scroll', this.onViewportUpdate);
+        window.addEventListener('resize', this.onViewportUpdate);
     }
 
     componentWillUnmount() {
-        window.removeEventListener('scroll', this.onScroll);
-    }
-
-    @bind
-    onScroll() {
-        if (this.containerElement) {
-            const endIndex = Math.ceil((window.innerHeight - this.containerElement.getBoundingClientRect().top) / this.averageElementHeight);
-            this.setState({endIndex: Math.max(endIndex, this.state.endIndex)});
-        }
-    }
-
-    shouldComponentUpdate(nextProps: Props<T>, nextState: State) {
-        return (nextState.endIndex > this.state.endIndex || (this.state.length == null && nextState.length != null));
+        window.removeEventListener('scroll', this.onViewportUpdate);
+        window.removeEventListener('resize', this.onViewportUpdate);
     }
 
 };
